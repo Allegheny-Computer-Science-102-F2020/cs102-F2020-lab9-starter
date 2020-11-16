4e7d499345dcbdf0e5540d6cdde96eadffc3b230
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 14:50:50 -0500

    Add the starting version of the fibonacci module with TODO markers.

---
+++ fibonaccicreator/fibonaccicreator/fibonacci.py
@@ -0,0 +1,30 @@
+"""Compute values in the Fibonacci sequence using different approaches."""
+
+# TODO: create an empty dictionary used for storage
+# of previously computed Fibonacci values
+
+
+def fibonacci_iterative(number: int) -> int:
+    """Compute the number-th Fibonacci number using iteration."""
+    # set the initial conditions for the calculation
+    a = 1
+    b = 1
+    # TODO: provide a complete implementation of this
+    # iterative function that returns the number-th Fibonacci value
+    return a
+
+
+def fibonacci_recursive(number: int) -> int:
+    """Compute the number-th Fibonacci number using recursion."""
+    # TODO: provide a complete implementation of this
+    # recursive function that returns the number-th Fibonacci value
+    # TODO: base case
+    # TODO: recursive case
+
+
+def fibonacci_memoized(number: int) -> int:
+    """Compute the number-th Fibonacci number using recursion."""
+    # TODO: provide a complete implementation of this memoized
+    # recursive function that returns the number-th Fibonacci value
+    # TODO: base case
+    # TODO: recursive case
